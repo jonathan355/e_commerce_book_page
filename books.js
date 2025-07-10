5c1252dbdf50c2a43a4ad17146afcdfe5c808eff
jonathan355 <jonathan35587@gmail.com>
2025-07-10 15:37:01 -0500

    added more js functionality

--- books.js
+++ books.js
@@ -1,14 +1,22 @@
-function renderBooks(filter) {
+let books;
+
+async function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
 
-  const books = getBooks();
+  booksWrapper.classList += ' books__loading'
+
+  if (!books) {
+    books = await getBooks();
+  }
+  
+  booksWrapper.classList.remove('books__loading')
 
   
   if (filter === 'LOW_TO_HIGH') {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
   }
   else if (filter === 'HIGH_TO_LOW') {
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
+    books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
   }
   else if (filter === 'RATING') {
   books.sort((a,b) => b.rating - a.rating);
@@ -28,7 +36,7 @@ function renderBooks(filter) {
               ${ratingsHTML(book.rating)}
             </div>
             <div class="book__price">
-              <span>$${book.originalPrice.toFixed(2)}</span>
+                ${priceHTML(book.originalPrice, book.salePrice)}
             </div>
     </div>`;
  })
@@ -37,13 +45,22 @@ function renderBooks(filter) {
  booksWrapper.innerHTML = booksHtml;
 }
 
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+  else {
+  return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`;
+}
+}
+
 function ratingsHTML(rating) {
 let ratingHTML = "";
 for (let i = 0; i < Math.floor(rating); ++i) {
-ratingHTML += '<i class="fa-solid fa-star"></i>\n'
+ratingHTML += '<i class="fas fa-star"></i>\n'
 }
 if (!Number.isInteger(rating)) {
-  ratingHTML += '<i class="fa-solid fa-star-half"></i>\n';
+  ratingHTML += '<i class="fas fa-star-half"></i>\n';
 }
 return ratingHTML;
 }
@@ -59,7 +76,9 @@ renderBooks();
 
 // FAKE DATA
 function getBooks() {
-  return [
+return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve ([
     {
       id: 1,
       title: "Crack the Coding Interview",
@@ -148,5 +167,8 @@ function getBooks() {
       salePrice: null,
       rating: 4.5,
     },
-  ];
+
+  ]);
+}, 1000);
+});
 }
